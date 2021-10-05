858f73dcd6fdd595a03505ef5fa5382dff40bb77
Prathamesh-gitacc <76098843+Prathamesh-gitacc@users.noreply.github.com>
2021-10-05 15:55:24 +0530

    Update story.js

--- story.js
+++ story.js
@@ -1,2 +1,3 @@
 Once upon a time in a forest there lived a monkey on a berries tree which was on the banks of river Nile.
 There was a crocdile in the river. His wife always want to eat a fresh meat full delicious and tasty. 
+Once he goes for finding some fruits for him and his wife,suddenly he saw a monkey in the berry tree.
